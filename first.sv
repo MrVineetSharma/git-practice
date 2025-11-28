5e5ad9915ad1d8a5fa8293940a96ce6d5ac407ff
Vineet Sharma <er.vineetsharma.bharatpur.1986@gmail.com>
2025-11-28 16:43:11 +0530

    second commit

--- first.sv
+++ first.sv
@@ -1,1 +1,37 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+class first extends uvm_sequence_item;   
+  rand logic[2:0]a=0;
+  rand logic[2:0]b=0;
+`uvm_object_utils_begin(first);
+`uvm_field_int(a,UVM_DEFAULT);
+`uvm_field_int(b,UVM_DEFAULT);
+`uvm_object_utils_end
+  
+function new(string path = "first");   
+super.new(path);
+`uvm_info("TB",$sformatf("i am in first class new function"),UVM_MEDIUM);
+endfunction
+  
+function void disp( );
+  `uvm_info("TB",$sformatf("first disp"),UVM_MEDIUM);
+  
+endfunction  
+
+endclass
+
+module tb();
+first f; 
+initial 
+begin
+`uvm_info("TB",$sformatf("i am in initial block"),UVM_MEDIUM); 
+  f = first::type_id::create("f");
+end
+
+ 
+endmodule
+
+
+
+
 //this is first file 
